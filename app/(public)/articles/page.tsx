be6a48f49259875257a41c8c47bc139034149b8e
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-07-07 19:32:53 +0700

    feat: add missing path

---
+++ app/(public)/articles/page.tsx
@@ -0,0 +1,9 @@
+"use server";
+import type { NextPage } from "next";
+import type { ReactElement } from "react";
+
+const ArticlesPage: NextPage = (): ReactElement => {
+  return <div>Articles Page</div>;
+};
+
+export default ArticlesPage;
---
+++ app/(public)/news/page.tsx
@@ -0,0 +1,9 @@
+"use server";
+import type { NextPage } from "next";
+import type { ReactElement } from "react";
+
+const NewsPage: NextPage = (): ReactElement => {
+  return <div>News Page</div>;
+};
+
+export default NewsPage;
