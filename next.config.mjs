cd08b7e56531f58cac2113967059b1d1ae33934e
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-07-07 15:51:45 +0700

    fix: argon2 error on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: { "/": ["./node_modules/argon2/prebuilds/linux-x64/*.musl.*"] },
+  },
+};
 
 export default nextConfig;
